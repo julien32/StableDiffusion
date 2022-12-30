d4ef8e969ebad229a88afeda03ef07a5658b2df7
julien32 <64914778+julien32@users.noreply.github.com>
2022-12-30 19:12:02 +0100

    enter command and submit

--- website/functions.py
+++ website/functions.py
@@ -4,10 +4,10 @@ from flask_login import login_required, current_user
 
 functions = Blueprint('functions', __name__)
 
-@functions.route('/run_text2Img')
+@functions.route('/run_text2Img/<string:prompt>')
 @login_required
-def run_text2Img():
-    command = "ls -l /Users/*"
+def run_text2Img(prompt):
+    command = prompt
     output = subprocess.run(command, shell=True, capture_output=True)
     print(output)
 
